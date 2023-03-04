f654bf7f6153fd6fff3bf2041be1f7ae77eedbd8
mcli007 <mchungkaili007@gmail.com>
2023-03-04 13:53:37 -0500

    api response update

--- backend/core/api/api_response.py
+++ backend/core/api/api_response.py
@@ -6,7 +6,7 @@ class Serializable:
         raise NotImplementedError()
 
 class APIResponse:
-    def __init__(self, is_error: bool, error_message:str, status: int, data: Type[Serializable]) -> None:
+    def __init__(self, is_error: bool, error_message:str, status: int, data: Type[Serializable]):
         self.is_error = is_error
         self.error_message = error_message
         self.status = status
@@ -44,9 +44,12 @@ class AddData(Serializable):
 class Battery(Serializable):
     
     data = {
-        'activated': False,
         'name': 'Battery',
+        'activated': False,
         'percent': 0,
+        'units': '%',
+        'time': 0,
+        'battery_health': 'Healthy',
     }
     @staticmethod
     def serialize(self)->dict:
@@ -55,11 +58,15 @@ class Battery(Serializable):
 class Temperature(Serializable):
 
     data = {
-        'activated': False,
         'name': 'Thermostat',
+        'activated': False,
         'readings_F': 0,
         'readings_C': 0,
+        'units_F': 'F',
+        'units_C': 'C',
+        'time': 0,
     }
+
     @staticmethod
     def serialize(self)->dict:
         return Temperature.data
@@ -67,18 +74,59 @@ class Temperature(Serializable):
 class Speed(Serializable):
     
     data = {
+        'name': 'Speed',
         'activated': False,
-        'name': 'Odometer',
+        'meter': 'Odometer',
         'speed_mph': 0,
         'speed_km': 0,
+        'units_mph': 'mph',
+        'units_km': 'km',
+        'time': 0,
     }
+
     @staticmethod
     def serialize(self)->dict:
         return Speed.data
     
-class Rotation:
+class Engine(Serializable):
     
     data = {
+        'name': 'Engine',
         'activated': False,
-        'name': 'Tachometer',
+        'meter': 'Tachometer',
+        'rpm': 0,
+        'units': 'rpm',
+        'time': 0,
+        'tire_health': 'Healthy',
+        'engine_health': 'Healthy',
     }
+
+    @staticmethod
+    def serialize(self)->dict:
+        return Engine.data
+
+class Solar(Serializable):
+
+    data = {
+        'name': 'Solar Energy Input',
+        'activated': False,
+        'solar_collection': 0,
+        'units': '%',
+        'solar_health': 'Healthy',
+        'time': 0,
+    }
+
+    @staticmethod
+    def serialize(self)->dict:
+        return Solar.data
+
+class Chassis(Serializable): #remove if no possible input for chasis integrity
+
+    data = {
+        'name': 'Chassis',
+        'chassis_health': 'Healthy', 
+    }   
+
+    @staticmethod
+    def serialize(self)->dict:
+        return Chassis.data
