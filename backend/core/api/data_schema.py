b90256a5d2b04c81dc8f99aa76667f011ec5c04a
Darin Tsai <darint916@gmail.com>
2023-04-01 13:46:21 -0400

    schema strict update

--- backend/core/api/data_schema.py
+++ backend/core/api/data_schema.py
@@ -1,45 +1,44 @@
 from cerberus import Validator
 
 battery_schema = {
-    'time': {'type': 'integer', 'required': True},
+    'time': {'type': 'datetime', 'required': True},
     'activated': {'type': 'boolean', 'required': True},
-    'percent': {'type': 'integer', 'required': True},
-    'units': {'type': 'string', 'required': True},
-    'battery_health': {'type': 'string', 'required': True},
+    'percent': {'type': 'integer', 'required': True, 'min': 0},
+    'battery_health': {'type': 'string', 'required': True, 'allowed': ['healthy', 'warning', 'critical']},
 }
 
 temperature_schema = {
-    'time': {'type': 'integer', 'required': True},
+    'time': {'type': 'datetime', 'required': True},
     'activated': {'type': 'boolean', 'required': True},
-    'readings_F': {'type': 'integer', 'required': True},
-    'readings_C': {'type': 'integer', 'required': True},
+    'temp_F': {'type': 'integer', 'required': True},
+    'temp_c': {'type': 'integer', 'required': True},
 }
 
 speed_schema = {
-    'time': {'type': 'integer', 'required': True},
+    'time': {'type': 'datetime', 'required': True},
     'activated': {'type': 'boolean', 'required': True},
     'mph': {'type': 'integer', 'required': True},
-    'km': {'type': 'integer', 'required': True},
+    'km': {'type': 'integer', 'required': True,},
 }
 
 engine_schema = {
-    'time': {'type': 'integer', 'required': True},
+    'time': {'type': 'datetime', 'required': True},
     'activated': {'type': 'boolean', 'required': True},
-    'rpm': {'type': 'integer', 'required': True},
-    'tire_health': {'type': 'string', 'required': True},
-    'engine_health': {'type': 'string', 'required': True},
+    'rpm': {'type': 'integer', 'required': True, 'min': 0},
+    'tire_health': {'type': 'string', 'required': True, 'allowed': ['healthy', 'warning', 'critical']},
+    'engine_health': {'type': 'string', 'required': True, 'allowed': ['healthy', 'warning', 'critical']},
 }
 
 solar_schema = {
-    'time': {'type': 'integer', 'required': True},
+    'time': {'type': 'datetime', 'required': True},
     'activated': {'type': 'boolean', 'required': True},
-    'solar_collection': {'type': 'integer', 'required': True},
-    'solar_health': {'type': 'string', 'required': True},
+    'power_generated': {'type': 'integer', 'required': True},
+    'solar_health': {'type': 'string', 'required': True, 'allowed': ['healthy', 'warning', 'critical']},
 }
 
 chasiss_schema = {
-    'time': {'type': 'integer', 'required': True},
-    'chassis_health': {'type': 'string', 'required': True},
+    'time': {'type': 'datetime', 'required': True},
+    'chassis_health': {'type': 'string', 'required': True, 'allowed': ['healthy', 'warning', 'critical']},
 }
 
 def validate_data(data, schema):
